c8fcb030466e79c04de0e83ea96ad9193612d49e
nickcen <cenyongh@gmail.com>
2019-03-21 09:40:39 +0800

    jsonrpc

--- provider.go
+++ provider.go
@@ -13,8 +13,132 @@ func NewProvider(host string) *Provider {
 	return &Provider{rpcClient}
 }
 
+func (provider *Provider) GetNetworkId() *jsonrpc.RPCResponse {
+	return provider.call("GetNetworkId")
+}
+
 func (provider *Provider) GetBlockchainInfo() *jsonrpc.RPCResponse {
 	return provider.call("GetBlockchainInfo")
+}
+
+func (provider *Provider) GetShardingStructure() *jsonrpc.RPCResponse {
+	return provider.call("GetShardingStructure")
+}
+
+func (provider *Provider) GetDsBlock() *jsonrpc.RPCResponse {
+	return provider.call("GetDsBlock")
+}
+
+func (provider *Provider) GetLatestDsBlock() *jsonrpc.RPCResponse {
+	return provider.call("GetLatestDsBlock")
+}
+
+func (provider *Provider) GetNumDSBlocks() *jsonrpc.RPCResponse {
+	return provider.call("GetNumDSBlocks")
+}
+
+func (provider *Provider) GetDSBlockRate() *jsonrpc.RPCResponse {
+	return provider.call("GetDSBlockRate")
+}
+
+func (provider *Provider) DSBlockListing() *jsonrpc.RPCResponse {
+	return provider.call("DSBlockListing")
+}
+
+func (provider *Provider) GetTxBlock() *jsonrpc.RPCResponse {
+	return provider.call("GetTxBlock")
+}
+
+func (provider *Provider) GetLatestTxBlock() *jsonrpc.RPCResponse {
+	return provider.call("GetLatestTxBlock")
+}
+
+func (provider *Provider) GetNumTxBlocks() *jsonrpc.RPCResponse {
+	return provider.call("GetNumTxBlocks")
+}
+
+func (provider *Provider) GetTxBlockRate() *jsonrpc.RPCResponse {
+	return provider.call("GetTxBlockRate")
+}
+
+func (provider *Provider) TxBlockListing() *jsonrpc.RPCResponse {
+	return provider.call("TxBlockListing")
+}
+
+func (provider *Provider) GetNumTransactions() *jsonrpc.RPCResponse {
+	return provider.call("GetNumTransactions")
+}
+
+func (provider *Provider) GetTransactionRate() *jsonrpc.RPCResponse {
+	return provider.call("GetTransactionRate")
+}
+
+func (provider *Provider) GetCurrentMiniEpoch() *jsonrpc.RPCResponse {
+	return provider.call("GetCurrentMiniEpoch")
+}
+
+func (provider *Provider) GetCurrentDSEpoch() *jsonrpc.RPCResponse {
+	return provider.call("GetCurrentDSEpoch")
+}
+
+func (provider *Provider) GetPrevDifficulty() *jsonrpc.RPCResponse {
+	return provider.call("GetPrevDifficulty")
+}
+
+func (provider *Provider) GetPrevDSDifficulty() *jsonrpc.RPCResponse {
+	return provider.call("GetPrevDSDifficulty")
+}
+
+func (provider *Provider) CreateTransaction() *jsonrpc.RPCResponse {
+	return provider.call("CreateTransaction")
+}
+
+func (provider *Provider) GetTransaction() *jsonrpc.RPCResponse {
+	return provider.call("GetTransaction")
+}
+
+func (provider *Provider) GetRecentTransactions() *jsonrpc.RPCResponse {
+	return provider.call("GetRecentTransactions")
+}
+
+func (provider *Provider) GetTransactionsForTxBlock() *jsonrpc.RPCResponse {
+	return provider.call("GetTransactionsForTxBlock")
+}
+
+func (provider *Provider) GetNumTxnsTxEpoch() *jsonrpc.RPCResponse {
+	return provider.call("GetNumTxnsTxEpoch")
+}
+
+func (provider *Provider) GetNumTxnsDSEpoch() *jsonrpc.RPCResponse {
+	return provider.call("GetNumTxnsDSEpoch")
+}
+
+func (provider *Provider) GetMinimumGasPrice() *jsonrpc.RPCResponse {
+	return provider.call("GetMinimumGasPrice")
+}
+
+func (provider *Provider) GetSmartContractCode() *jsonrpc.RPCResponse {
+	return provider.call("GetSmartContractCode")
+}
+
+func (provider *Provider) GetSmartContractInit() *jsonrpc.RPCResponse {
+	return provider.call("GetSmartContractInit")
+}
+
+func (provider *Provider) GetSmartContractState() *jsonrpc.RPCResponse {
+	return provider.call("GetSmartContractState")
+}
+
+func (provider *Provider) GetSmartContracts() *jsonrpc.RPCResponse {
+	return provider.call("GetSmartContracts")
+}
+
+func (provider *Provider) GetContractAddressFromTransactionID() *jsonrpc.RPCResponse {
+	return provider.call("GetContractAddressFromTransactionID")
+}
+
+func (provider *Provider) GetBalance() *jsonrpc.RPCResponse {
+	return provider.call("GetBalance")
 }
 
 func (provider *Provider) call(method_name string) *jsonrpc.RPCResponse {
--- provider_test.go
+++ provider_test.go
@@ -5,8 +5,226 @@ import (
 	"testing"
 )
 
+func TestGetNetworkId(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetNetworkId()
+	fmt.Printf("%v\n", response)
+}
+
 func TestGetBlockchainInfo(t *testing.T) {
 	provider := NewProvider("https://dev-api.zilliqa.com/")
 
 	response := provider.GetBlockchainInfo()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetShardingStructure(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetShardingStructure()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetDsBlock(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetDsBlock()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetLatestDsBlock(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetLatestDsBlock()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetNumDSBlocks(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetNumDSBlocks()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetDSBlockRate(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetDSBlockRate()
+	fmt.Printf("%v\n", response)
+}
+
+func TestDSBlockListing(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.DSBlockListing()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetTxBlock(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetTxBlock()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetLatestTxBlock(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetLatestTxBlock()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetNumTxBlocks(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetNumTxBlocks()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetTxBlockRate(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetTxBlockRate()
+	fmt.Printf("%v\n", response)
+}
+
+func TestTxBlockListing(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.TxBlockListing()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetNumTransactions(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetNumTransactions()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetTransactionRate(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetTransactionRate()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetCurrentMiniEpoch(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetCurrentMiniEpoch()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetCurrentDSEpoch(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetCurrentDSEpoch()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetPrevDifficulty(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetPrevDifficulty()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetPrevDSDifficulty(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetPrevDSDifficulty()
+	fmt.Printf("%v\n", response)
+}
+
+func TestCreateTransaction(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.CreateTransaction()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetTransaction(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetTransaction()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetRecentTransactions(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetRecentTransactions()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetTransactionsForTxBlock(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetTransactionsForTxBlock()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetNumTxnsTxEpoch(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetNumTxnsTxEpoch()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetNumTxnsDSEpoch(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetNumTxnsDSEpoch()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetMinimumGasPrice(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetMinimumGasPrice()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetSmartContractCode(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetSmartContractCode()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetSmartContractInit(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetSmartContractInit()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetSmartContractState(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetSmartContractState()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetSmartContracts(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetSmartContracts()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetContractAddressFromTransactionID(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetContractAddressFromTransactionID()
+	fmt.Printf("%v\n", response)
+}
+
+func TestGetBalance(t *testing.T) {
+	provider := NewProvider("https://dev-api.zilliqa.com/")
+
+	response := provider.GetBalance()
+	fmt.Printf("%v\n", response)
 }
