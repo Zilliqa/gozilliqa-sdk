8319205a12b8b59ece25b0cb847ed7a6869e153c
xiaohuo <xiaohuo200@gmail.com>
2019-04-10 13:37:45 +0800

    feat(keytools): generate private key

---
+++ keytools/secp256k1.go
@@ -0,0 +1,18 @@
+package keytools
+
+import (
+	"crypto/rand"
+	"io"
+)
+
+type PrivateKey [32]byte
+
+func GeneratePrivateKey() (PrivateKey, error) {
+	pvk := [32]byte{}
+	_, err := io.ReadFull(rand.Reader, pvk[:])
+
+	if err != nil {
+		return pvk, err
+	}
+	return PrivateKey(pvk), nil
+}
