d6009273a7884212a2a896b249d4993baf6edb74
xiaohuo <xiaohuo200@gmail.com>
2019-05-03 01:49:09 +0800

    feat(transaction): encode transaction proto

---
+++ transaction/transaction_util.go
@@ -0,0 +1,83 @@
+package transaction
+
+import (
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"github.com/FireStack-Lab/LaksaGo"
+	"github.com/FireStack-Lab/LaksaGo/protobuf"
+	"github.com/golang/protobuf/proto"
+	"math/big"
+	"strconv"
+)
+
+func EncodeTransactionProto(txParams TxParams) ([]byte, error) {
+	amount, ok := new(big.Int).SetString(txParams.Amount, 10)
+	if !ok {
+		return nil, errors.New("amount error")
+	}
+
+	gasPrice, ok2 := new(big.Int).SetString(txParams.GasPrice, 10)
+	if !ok2 {
+		return nil, errors.New("gas price error")
+	}
+
+	v, err := strconv.ParseUint(txParams.Version, 10, 32)
+	if err != nil {
+		return nil, err
+	}
+	version := uint32(v)
+
+	nonce, err2 := strconv.ParseUint(txParams.Nonce, 10, 64)
+	if err2 != nil {
+		return nil, err2
+	}
+
+	senderpubkey := protobuf.ByteArray{
+		Data: LaksaGo.DecodeHex(txParams.SenderPubKey),
+	}
+
+	amountArray := protobuf.ByteArray{
+		Data: bigIntToPaddedBytes(amount, 32),
+	}
+
+	gasPriceArray := protobuf.ByteArray{
+		Data: bigIntToPaddedBytes(gasPrice, 32),
+	}
+
+	gasLimit, err3 := strconv.ParseUint(txParams.GasLimit, 10, 64)
+	if err3 != nil {
+		return nil, err3
+	}
+
+	protoTransactionCoreInfo := protobuf.ProtoTransactionCoreInfo{
+		Version:      &version,
+		Nonce:        &nonce,
+		Toaddr:       LaksaGo.DecodeHex(txParams.ToAddr),
+		Senderpubkey: &senderpubkey,
+		Amount:       &amountArray,
+		Gasprice:     &gasPriceArray,
+		Gaslimit:     &gasLimit,
+	}
+
+	if txParams.Data != "" {
+		protoTransactionCoreInfo.Data = LaksaGo.DecodeHex(txParams.Data)
+	}
+
+	if txParams.Code != "" {
+		protoTransactionCoreInfo.Data = LaksaGo.DecodeHex(txParams.Code)
+	}
+
+	bytes, err4 := proto.Marshal(&protoTransactionCoreInfo)
+	if err4 != nil {
+		return nil, err4
+	}
+	return bytes, nil
+
+}
+
+func bigIntToPaddedBytes(i *big.Int, paddedSize int32) []byte {
+	bytes := i.Bytes()
+	padded, _ := hex.DecodeString(fmt.Sprintf("%0*x", paddedSize, bytes))
+	return padded
+}
---
+++ transaction/transaction_util_test.go
@@ -0,0 +1,26 @@
+package transaction
+
+import (
+	"github.com/FireStack-Lab/LaksaGo"
+	"strings"
+	"testing"
+)
+
+func TestEncodeTransactionProto(t *testing.T) {
+	txParams := TxParams{
+		Version:      "0",
+		Nonce:        "0",
+		ToAddr:       "2E3C9B415B19AE4035503A06192A0FAD76E04243",
+		SenderPubKey: "0246e7178dc8253201101e18fd6f6eb9972451d121fc57aa2a06dd5c111e58dc6a",
+		Amount:       "10000",
+		GasPrice:     "100",
+		GasLimit:     "1000",
+		Code:         "",
+		Data:         "",
+	}
+
+	bytes, _ := EncodeTransactionProto(txParams)
+	if strings.Compare(strings.ToUpper(LaksaGo.EncodeHex(bytes)),"080010001A142E3C9B415B19AE4035503A06192A0FAD76E0424322230A210246E7178DC8253201101E18FD6F6EB9972451D121FC57AA2A06DD5C111E58DC6A2A120A100000000000000000000000000000271032120A100000000000000000000000000000006438E807") != 0 {
+		t.Error("encode error")
+	}
+}
