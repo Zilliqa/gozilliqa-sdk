96275afb53641d68b1ec64246cbb2248fee98360
xiaohuo <xiaohuo200@gmail.com>
2020-01-16 16:31:32 +0800

    chore: add event log field for transaction receipt

--- transaction/transaction_receipt.go
+++ transaction/transaction_receipt.go
@@ -4,4 +4,5 @@ type TransactionReceipt struct {
 	Success       bool `json:"success"`
 	CumulativeGas string `json:"cumulative_gas"`
 	EpochNum      string `json:"epoch_num"`
+	EventLogs []interface{} `json:"event_logs"`
 }
---
+++ transaction/transaction_receipt_test.go
@@ -0,0 +1,52 @@
+package transaction
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func TestTransactionReceipt(t *testing.T) {
+	js := []byte(`
+		{
+            "cumulative_gas": "1110",
+            "epoch_num": "324",
+            "event_logs": [
+                {
+                    "_eventname": "sorted",
+                    "address": "0x297241320bfd1796efb1780e7a6732dfbe93220f",
+                    "params": [
+                        {
+                            "type": "List (Uint32)",
+                            "value": [
+                                "1",
+                                "2"
+                            ],
+                            "vname": "sorted"
+                        }
+                    ]
+                },
+                {
+                    "_eventname": "unsorted",
+                    "address": "0x297241320bfd1796efb1780e7a6732dfbe93220f",
+                    "params": [
+                        {
+                            "type": "List (Uint32)",
+                            "value": [
+                                "2",
+                                "1"
+                            ],
+                            "vname": "unsorted"
+                        }
+                    ]
+                }
+            ],
+            "success": true
+        }`)
+	var receipt TransactionReceipt
+	err := json.Unmarshal(js,&receipt)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	fmt.Println(receipt)
+}
