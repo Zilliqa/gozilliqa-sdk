9fa6cabc7044103eba1468dcc3546457aa8cd460
xiaohuo <xiaohuo200@gmail.com>
2020-11-27 15:53:40 +0800

    feat(multisig): verify

--- multisig/multisig.go
+++ multisig/multisig.go
@@ -1,11 +1,18 @@
 package multisig
 
 import (
-	"crypto/elliptic"
+	"bytes"
 	"errors"
+	"fmt"
 	"github.com/Zilliqa/gozilliqa-sdk/keytools"
+	"github.com/Zilliqa/gozilliqa-sdk/util"
 	"github.com/btcsuite/btcd/btcec"
+	"math/big"
 )
+
+var bintZero = big.NewInt(0)
+const ThirdDomainSeparatedHashFunctionByte = 0x11
+
 
 func AggregatedPubKey(pubKeys [][]byte) ([]byte, error) {
 	if len(pubKeys) == 0 {
@@ -23,7 +30,7 @@ func AggregatedPubKey(pubKeys [][]byte) ([]byte, error) {
 			return nil, err1
 		}
 		x, y := keytools.Secp256k1.Add(aggregatedPubKey.X, aggregatedPubKey.Y, puk.X, puk.Y)
-		bytes := elliptic.MarshalCompressed(keytools.Secp256k1, x, y)
+		bytes := util.Marshal(keytools.Secp256k1, x, y, true)
 		pubKey, err2 := btcec.ParsePubKey(bytes, keytools.Secp256k1)
 		if err2 != nil {
 			return nil, err2
@@ -34,4 +41,55 @@ func AggregatedPubKey(pubKeys [][]byte) ([]byte, error) {
 	}
 
 	return aggregatedPubKey.SerializeCompressed(), nil
+}
+
+func MultiVerify(publicKey []byte, msg []byte, r []byte, s []byte) bool {
+	bintR := new(big.Int).SetBytes(r)
+	bintS := new(big.Int).SetBytes(s)
+
+	//cannot be zero
+	if bintR.Cmp(bintZero) == 0 || bintS.Cmp(bintZero) == 0 {
+		fmt.Printf("Invalid R or S value: cannot be zero")
+		return false
+	}
+
+	//cannot be negative
+	if bintR.Sign() == -1 || bintS.Sign() == -1 {
+		fmt.Printf("Invalid R or S value: cannot be negative")
+		return false
+	}
+
+	// cannot be greater than curve.N
+	if bintR.Cmp(keytools.Secp256k1.N) == 1 || bintS.Cmp(keytools.Secp256k1.N) == 1 {
+		fmt.Printf("Invalid R or S value: cannot be greater than order of secp256k1")
+		return false
+	}
+
+	puk, err := btcec.ParsePubKey(publicKey, keytools.Secp256k1)
+
+	if err != nil {
+		panic("parse public key error")
+	}
+
+	pkx, pky := puk.X, puk.Y
+
+	lx, ly := keytools.Secp256k1.ScalarMult(pkx, pky, r)
+	rx, ry := keytools.Secp256k1.ScalarBaseMult(s)
+	Qx, Qy := keytools.Secp256k1.Add(rx, ry, lx, ly)
+	Q := util.Compress(keytools.Secp256k1, Qx, Qy, true)
+
+	_r := hash(0x11,Q,publicKey,msg)
+	_rn := new(big.Int).Mod(new(big.Int).SetBytes(_r), keytools.Secp256k1.N)
+
+	rn := new(big.Int).SetBytes(r)
+	return rn.Cmp(_rn) == 0
+}
+
+func hash(first byte, Q []byte, pubKey []byte, msg []byte) []byte {
+	var buffer bytes.Buffer
+	buffer.WriteByte(first)
+	buffer.Write(Q)
+	buffer.Write(pubKey[:])
+	buffer.Write(msg)
+	return util.Sha256(buffer.Bytes())
 }
