6072ca15e1d89e7422c1b56ab6b646dfe0258fa9
xiaohuo <xiaohuo200@gmail.com>
2020-12-24 19:31:48 +0800

    feat(polynetwork): split signature

--- crosschain/polynetwork/util.go
+++ crosschain/polynetwork/util.go
@@ -6,19 +6,28 @@ import (
 )
 
 const polyChainPubKeyLen = 67 * 2
+const polyChainSignatureLen = 65 * 2
 
 func SplitPubKeys(rawBytes string) ([]string, error) {
+	return split(rawBytes, polyChainPubKeyLen)
+}
+
+func SplitSignature(rawBytes string) ([]string, error) {
+	return split(rawBytes, polyChainSignatureLen)
+}
+
+func split(rawBytes string, l int) ([]string, error) {
 	if strings.HasPrefix(rawBytes, "0x") {
 		rawBytes = rawBytes[2:]
 	}
 	var keys []string
 	keyLen := len(rawBytes)
-	if keyLen%polyChainPubKeyLen != 0 {
+	if keyLen%l != 0 {
 		return keys, errors.New("wrong length of public key list")
 	}
-	n := keyLen / polyChainPubKeyLen
+	n := keyLen / l
 	for i := 0; i < n; i++ {
-		publicKey := rawBytes[i*polyChainPubKeyLen : (i+1)*polyChainPubKeyLen]
+		publicKey := rawBytes[i*l : (i+1)*l]
 		keys = append(keys, "0x"+publicKey)
 	}
 	return keys, nil
