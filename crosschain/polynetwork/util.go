d4429b9022baec4f130749945d88a1c2037b5594
xiaohuo <xiaohuo200@gmail.com>
2020-12-24 13:34:33 +0800

    feat(polynetwork): handle 0x for keys

--- crosschain/polynetwork/util.go
+++ crosschain/polynetwork/util.go
@@ -1,19 +1,25 @@
 package polynetwork
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 const polyChainPubKeyLen = 67
 
 func SplitPubKeys(rawBytes string) ([]string, error) {
+	if strings.HasPrefix(rawBytes,"0x") {
+		rawBytes = rawBytes[2:]
+	}
 	var keys []string
 	keyLen := len(rawBytes)
-	if keyLen%polyChainPubKeyLen != 0 {
+	if keyLen % polyChainPubKeyLen != 0 {
 		return keys, errors.New("wrong length of public key list")
 	}
 	n := keyLen / polyChainPubKeyLen
 	for i := 0; i < n; i++ {
 		publicKey := rawBytes[i*polyChainPubKeyLen : (i+1)*polyChainPubKeyLen]
-		keys = append(keys, publicKey)
+		keys = append(keys, "0x" + publicKey)
 	}
 	return keys, nil
 }
