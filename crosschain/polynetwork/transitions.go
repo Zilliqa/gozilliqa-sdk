160efd161f678318877778bd06683a4f2c882cd3
xiaohuo <xiaohuo200@gmail.com>
2020-12-24 19:12:12 +0800

    feat: ChangeBookKeeper and VerifyHeaderAndExecuteTx

--- crosschain/polynetwork/transitions.go
+++ crosschain/polynetwork/transitions.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Zilliqa/gozilliqa-sdk/contract"
 	"github.com/Zilliqa/gozilliqa-sdk/core"
 	"github.com/Zilliqa/gozilliqa-sdk/provider"
+	"github.com/Zilliqa/gozilliqa-sdk/transaction"
 	"github.com/Zilliqa/gozilliqa-sdk/util"
 	"strconv"
 )
@@ -22,15 +23,25 @@ type Proxy struct {
 	MsgVersion int
 }
 
-func (p *Proxy) call(args []core.ContractValue, transition string) error {
+type ProofPair struct {
+	Key  string
+	Hash string
+}
+
+type ProofEntity struct {
+	Proof string
+	Pair  []ProofPair
+}
+
+func (p *Proxy) call(args []core.ContractValue, transition string) (*transaction.Transaction, error) {
 	bech32Addr, err := bech32.ToBech32Address(p.ProxyAddr)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	gasPrice, err1 := p.Client.GetMinimumGasPrice()
 	if err1 != nil {
-		return err1
+		return nil, err1
 	}
 
 	c := contract.Contract{
@@ -49,17 +60,17 @@ func (p *Proxy) call(args []core.ContractValue, transition string) error {
 
 	tx, err2 := c.Call(transition, args, params, true)
 	if err2 != nil {
-		return err2
+		return nil, err2
 	}
 	tx.Confirm(tx.ID, 1000, 10, p.Client)
 	if tx.Status == core.Confirmed {
-		return nil
+		return tx, nil
 	}
 
-	return errors.New(fmt.Sprintf("call %s failed", transition))
+	return nil, errors.New(fmt.Sprintf("call %s failed", transition))
 }
 
-func (p *Proxy) UpgradeTo() error {
+func (p *Proxy) UpgradeTo() (*transaction.Transaction, error) {
 
 	args := []core.ContractValue{
 		{
@@ -72,13 +83,13 @@ func (p *Proxy) UpgradeTo() error {
 	return p.call(args, "UpgradeTo")
 }
 
-func (p *Proxy) Unpause() error {
+func (p *Proxy) Unpause() (*transaction.Transaction, error) {
 	args := []core.ContractValue{}
 
 	return p.call(args, "UnPause")
 }
 
-func (p *Proxy) InitGenesisBlock(rawHeader string, pubKeys []string) error {
+func (p *Proxy) InitGenesisBlock(rawHeader string, pubKeys []string) (*transaction.Transaction, error) {
 	var keys []core.ParamConstructor
 	for _, key := range pubKeys {
 		keys = append(keys, core.ParamConstructor{
@@ -102,4 +113,103 @@ func (p *Proxy) InitGenesisBlock(rawHeader string, pubKeys []string) error {
 	}
 
 	return p.call(args, "InitGenesisBlock")
+}
+
+func (p *Proxy) ChangeBookKeeper(rawHeader string, pubKeys []string, sigList []string) (*transaction.Transaction, error) {
+	var keys []core.ParamConstructor
+	for _, key := range pubKeys {
+		keys = append(keys, core.ParamConstructor{
+			Constructor: "Polynetwork.Pubkey",
+			ArgTypes:    make([]interface{}, 0),
+			Arguments:   []string{key},
+		})
+	}
+
+	var sigs []core.ParamConstructor
+	for _, sig := range sigList {
+		sigs = append(sigs, core.ParamConstructor{
+			Constructor: "Polynetwork.Signature",
+			ArgTypes:    make([]interface{}, 0),
+			Arguments:   []string{sig},
+		})
+	}
+	args := []core.ContractValue{
+		{
+			"rawHeader",
+			"ByStr",
+			rawHeader,
+		},
+		{
+			"pubkeys",
+			"List Polynetwork.Pubkey",
+			keys,
+		},
+		{
+			"sigList",
+			"List Polynetwork.Signature",
+			sigs,
+		},
+	}
+
+	return p.call(args, "ChangeBookKeeper")
+}
+
+func (p *Proxy) VerifyHeaderAndExecuteTx(proof ProofEntity, rawHeader string, headerProof ProofEntity, curRawHeader string, headerSig []string) (*transaction.Transaction, error) {
+	var pairs []core.ParamConstructor
+
+	for _, pair := range proof.Pair {
+		pairs = append(pairs, core.ParamConstructor{
+			Constructor: "Pair",
+			ArgTypes:    []interface{}{"ByStr1", "ByStr32"},
+			Arguments:   []string{pair.Key, pair.Hash},
+		})
+	}
+
+	var headerPairs []core.ParamConstructor
+	for _, pair := range headerProof.Pair {
+		headerPairs = append(headerPairs, core.ParamConstructor{
+			Constructor: "Pair",
+			ArgTypes:    []interface{}{"ByStr1", "ByStr32"},
+			Arguments:   []string{pair.Key, pair.Hash},
+		})
+	}
+
+	var sigs []core.ParamConstructor
+	for _, sig := range headerSig {
+		sigs = append(sigs, core.ParamConstructor{
+			Constructor: "Polynetwork.Signature",
+			ArgTypes:    make([]interface{}, 0),
+			Arguments:   []string{sig},
+		})
+	}
+
+	args := []core.ContractValue{
+		{
+			"proof",
+			"Polynetwork.Proof",
+			pairs,
+		},
+		{
+			"rawHeader",
+			"ByStr",
+			rawHeader,
+		},
+		{
+			"headerProof",
+			"Polynetwork.Proof",
+			headerPairs,
+		},
+		{
+			"curRawHeader",
+			"ByStr",
+			curRawHeader,
+		},
+		{
+			"headerSig",
+			"List Polynetwork.Signature",
+			sigs,
+		},
+	}
+
+	return p.call(args, "VerifyHeaderAndExecuteTx")
 }
