dfd3a847dbe6637c2fab5613d63108d6f77d6444
Antonio Nunez <ansnunez@gmail.com>
2021-04-14 16:57:03 +0800

    Updated api

--- contract/contract.go
+++ contract/contract.go
@@ -34,7 +34,7 @@ const MainNet = "mainnet"
 const TestNet = "testnet"
 const Isolated = "isolated"
 const TestNetHost = "https://dev-api.zilliqa.com/"
-const MainNetHost = "https://tinyv800alpha1-api.dev.z7a.xyz/"
+const MainNetHost = "https://tinyv800alpha12-api.dev.z7a.xyz/"
 const IsolatedHost = "https://zilliqa-isolated-server.zilliqa.com/"
 
 const (
