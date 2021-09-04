925bfdcd39efe5266211675375e2f991369db27e
xiaohuo <xiaohuo200@gmail.com>
2021-09-04 09:42:56 +0800

    style: format code

--- crosschain/polynetwork/zero_copy_source.go
+++ crosschain/polynetwork/zero_copy_source.go
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package polynetwork
 
 import (
